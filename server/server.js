7b6e3f6a851cb0c316b4d5d62d53bc6fbb30bedf
Louay Elbiche <louay@Louays-MacBook-Pro.local>
2019-09-24 13:38:07 -0400

    Change server port to heroku

--- server/server.js
+++ server/server.js
@@ -83,7 +83,10 @@ function retrieveMessages(res) {
     }
   );
 }
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 9000;
 
-app.listen(9000, function() {
-  console.log("server is running on port 9000");
+app.listen(port, function() {
+  console.log("server is running on port ", port);
 });
